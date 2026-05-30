7436d59e6011ab9a9d7002d39d8fe1a899e13b28
Eyad Mohamed <eyad.ashdief@gmail.com>
2026-05-30 21:10:56 +0200

    Build hero section

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -4,9 +4,25 @@ function Browse() {
     return (
         <main>
             <Navbar />
-            <section>
-                <h1>DormMarket</h1>
+            <section className="hero-section">
+                
+                <div className="hero-badge">
+                    <span className="badge-dot"></span>
+                    Your personal marketplace
+                </div>
+                
+                <h1>
+                    Dorm<span className="hero-market">Market</span>
+                </h1>
+                
                 <p>Buy & sell within your student residence</p>
+
+                <input 
+                  className="hero-search" 
+                  type="text" 
+                  placeholder="What are you looking for?"
+                />
+            
             </section>
         </main>
     );
